015250296121a7d301d7ce24b18444237dc7e876
figoxu <xujh945@qq.com>
2017-11-30 20:29:25 +0800

    Add Print Methods

--- base/grammar/hello.rb
+++ base/grammar/hello.rb
@@ -1,1 +1,22 @@
 puts "Hello World!"
+
+print <<EOF
+This is the first way of creating
+here document ie. multiple line string.
+EOF
+
+print <<"EOF";                # same as above
+This is the second way of creating
+here document ie. multiple line string.
+EOF
+
+print <<`EOC`                 #  execute commands
+ls -l
+whoami
+EOC
+
+print <<"foo", <<"bar"   # you can stack them
+I said foo.
+foo
+I said bar.
+bar
