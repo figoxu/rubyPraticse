91eefcf3775cf17899e5fe74033c913d86161c0f
figoxu <xujh945@qq.com>
2017-12-02 18:58:16 +0800

    Add If Sample

--- base/grammar/loop.rb
+++ base/grammar/loop.rb
@@ -14,3 +14,16 @@ when 13 .. 18
 else 
     puts "adult"
 end
+
+
+$debug = false
+print "debug\n" if $debug
+print "product\n" if !$debug
+
+x = 1
+if x > 2
+    puts "x is greater than 2"
+else
+    puts "I can't guess the number"
+end
+
