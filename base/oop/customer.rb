6b51640ac7ee407524ffba948d61a2566a07df1d
figoxu <xujh945@qq.com>
2017-12-02 11:29:18 +0800

    Add Global Variable Sample

--- base/oop/customer.rb
+++ base/oop/customer.rb
@@ -5,7 +5,7 @@
 #以 $ 开始的是全局变量
 #以小写或者是 _ 开头的是局部变量
 
-
+$global_var = 10 #定义全局变量
 class Customer
 
     @@no_of_customer=0
@@ -23,6 +23,11 @@ class Customer
         @@no_of_customer +=1
         puts "Total number of customers: #@@no_of_customer"
     end
+
+    def print_global()
+        puts "Global Variable Is #$global_var"
+        #通过在变量或常量的前面放置”#”，可以实现对任何变量或常量的访问
+    end
 end
 
 cust = Customer.new("Figo","$1000")
@@ -30,3 +35,4 @@ cust.hello()
 cust.total_no_of_customer()
 cust.total_no_of_customer()
 cust.total_no_of_customer()
+cust.print_global()
