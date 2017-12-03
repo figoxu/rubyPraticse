a51f02a173d4703a08d11f5ba2b5da5a7e7e6651
figoxu <xujh945@qq.com>
2017-12-03 08:44:00 +0800

    增加unless 的范例

---
+++ base/grammar/judge.rb
@@ -0,0 +1,15 @@
+$var = true
+print "true -- Value is set \n" if $var
+print "false -- Value is set \n" unless $var
+
+
+$var2 = false
+
+print "Current Value Is False \n" unless $var2
+
+x = 1
+unless x>2
+    puts "x is less or equal than 2"
+else
+    puts "x is greater than 2"
+end
