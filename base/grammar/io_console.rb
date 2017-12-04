0179fa0a886ebec06f44ca37e166e4ecab0f098a
figoxu <xujh945@qq.com>
2017-12-04 08:53:02 +0800

    Add IO Of Console

---
+++ base/grammar/io_console.rb
@@ -0,0 +1,9 @@
+v = "hello"
+puts v #Print All String , End With New Line
+putc v #Print First Char ,End WithOut New Line
+puts "=="
+print "Hello World" #Print String ,End WithOut New Line
+print "==\n"
+puts "What Is Your Name ?"
+words = gets
+puts "Your Name Is #{ words }"
