a1f9e72727e4d934e56266b35337e7b59ed87697
figoxu <xujh945@qq.com>
2017-12-04 18:49:21 +0800

    增加Model的校验

--- rails/blog/app/models/post.rb
+++ rails/blog/app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many :comments
+  validates :title, presence: true, length: {maximum: 20}, uniqueness: true
+  validates :text, presence: true, length: {minmum: 50, maximum: 20000}
 end
--- rails/blog/app/models/user.rb
+++ rails/blog/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :name, presence: true, length: {minimum: 2, maximum: 20}, uniqueness: true
+  validates :email, presence: true, email: true, uniqueness: true
+  validates :password, presence: true, length: {minimum: 6, maximum: 16}
+  has_many :posts
 end
