64654823ea449a1a6f473a9883e8c0283ac46dc9
figoxu <xujh945@qq.com>
2018-03-24 17:11:37 +0800

    remove blog

--- rails/blog/app/models/post.rb
+++
@@ -1,6 +0,0 @@
-class Post < ApplicationRecord
-  belongs_to :user
-  has_many :comments
-  validates :title, presence: true, length: {maximum: 20}, uniqueness: true
-  validates :text, presence: true, length: {minmum: 50, maximum: 20000}
-end
