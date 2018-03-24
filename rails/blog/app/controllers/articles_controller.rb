69718bfe964d23d01f80661e9186fe2151f4b54f
figoxu <xujh945@qq.com>
2018-03-24 21:53:05 +0800

    增加表单验证

--- rails/blog/app/controllers/articles_controller.rb
+++ rails/blog/app/controllers/articles_controller.rb
@@ -5,19 +5,16 @@ class ArticlesController < ApplicationController
     end
 
     def new
+      @article = Article.new
     end
 
     def create
-        # c=params[:article]
-        # render plain: params[:article].inspect
-        # puts "debug >>>>A00001 #{c}"
-        Article.new(article_params)
-        puts "debug >>>>A00002"
         @article = Article.new(article_params)
-        puts "debug >>>>A00003"
-        @article.save
-        puts "debug >>>>A00004"
-        redirect_to @article
+        if @article.save
+          redirect_to @article
+        else
+          render 'new'
+        end
     end
 
     def show
