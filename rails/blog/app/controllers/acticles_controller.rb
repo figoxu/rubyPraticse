26651d45b5de3800cc38c3f1b8ba5e415cf49d80
figoxu <xujh945@qq.com>
2018-03-25 10:18:25 +0800

    同步git

--- rails/blog/app/controllers/acticles_controller.rb
+++
@@ -1,2 +0,0 @@
-class ActiclesController < ApplicationController
-end
--- rails/blog/test/controllers/acticles_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ActiclesControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
