d73cb108839f473364979d081a3438e565f7cc71
figoxu <xujh945@qq.com>
2018-03-27 21:45:41 +0800

    增加用户注册功能

--- rails/master_rails_by_actions/app/models/user.rb
+++ rails/master_rails_by_actions/app/models/user.rb
@@ -10,16 +10,16 @@ class User < ApplicationRecord
                         if: :need_validate_password
   validates_presence_of :password_confirmation, message: "密码确认不能为空",
                         if: :need_validate_password
-  validates_confirmation_of :passwod, message: "密码不一致",
+  validates_confirmation_of :password, message: "密码不一致",
                             if: :need_validate_password
   validates_length_of :password, message: "密码最短为6位", minimum: 6,
                       if: :need_validate_password
 
 
   private
-  def need_validate_passwod
+  def need_validate_password
     self.new_record? ||
-    (!self.passwod.nil? || !self.password_confirmation.nil?)
+    (!self.password.nil? || !self.password_confirmation.nil?)
   end
 
 end
