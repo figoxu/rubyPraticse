32a809828ead4630709bde34fa78cb529a57ec37
figoxu <xujh945@qq.com>
2018-03-29 10:00:40 +0800

    使用before_action过滤器，精简代码

--- rails/master_rails_by_actions/app/controllers/admin/categories_controller.rb
+++ rails/master_rails_by_actions/app/controllers/admin/categories_controller.rb
@@ -1,16 +1,18 @@
 class Admin::CategoriesController < Admin::BaseController
+  before_action :find_root_categories, only: [:new, :create, :edit, :update]
+  before_action :find_category, only: [:edit,:update,:destroy]
+
+
   def index
     @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
   end
 
   def new
     @category = Category.new
-    @root_categories = Category.roots.order(id: "desc")
   end
 
   def create
     @category = Category.new(params.require(:category).permit!)
-    @root_categories = Category.roots.order(id: "desc")
     if @category.save
       flash[:notice] = "保存成功"
       redirect_to admin_categories_path
@@ -20,14 +22,10 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def edit
-    @category = Category.find(params[:id])
-    @root_categories = Category.roots.order(id: "desc")
     render action: :new
   end
 
   def update
-    @category = Category.find(params[:id])
-    @root_categories = Category.roots.order(id: "desc")
     @category.attributes = params.require(:category).permit!
     if @category.save
       flash[:notice] = "修改成功"
@@ -38,14 +36,23 @@ class Admin::CategoriesController < Admin::BaseController
   end
 
   def destroy
-    @category = Category.find(params[:id])
     if @category.destroy
       flash[:notice] = "删除成功"
       redirect_to admin_categories_path
     else
-      flash[:notice]="删除失败"
+      flash[:notice] = "删除失败"
       redirect_to :back
     end
+  end
+
+
+  private
+  def find_root_categories
+    @root_categories = Category.roots.order(id: "desc")
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
   end
 
 end
