26740b7e1b84e4f49f780b21fd2197e77f008b04
figoxu <xujh945@qq.com>
2018-03-29 09:40:56 +0800

    增加分类编辑功能

--- rails/master_rails_by_actions/app/controllers/admin/categories_controller.rb
+++ rails/master_rails_by_actions/app/controllers/admin/categories_controller.rb
@@ -1,6 +1,6 @@
 class Admin::CategoriesController < Admin::BaseController
   def index
-    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id:"desc")
+    @categories = Category.roots.page(params[:page] || 1).per_page(params[:per_page] || 10).order(id: "desc")
   end
 
   def new
@@ -12,7 +12,24 @@ class Admin::CategoriesController < Admin::BaseController
     @category = Category.new(params.require(:category).permit!)
     @root_categories = Category.roots.order(id: "desc")
     if @category.save
-      flash[:notice]="保存成功"
+      flash[:notice] = "保存成功"
+      redirect_to admin_categories_path
+    else
+      render action: :new
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+    @root_categories = Category.roots.order(id: "desc")
+    render action: :new
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    @category.attributes = params.require(:category).permit!
+    if @category.save
+      flash[:notice] = "修改成功"
       redirect_to admin_categories_path
     else
       render action: :new
