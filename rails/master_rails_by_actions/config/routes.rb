8c63301856fa063cd118dca7cdd514900aaf5e34
figoxu <xujh945@qq.com>
2018-03-29 08:14:37 +0800

    创建管理系统Controller

--- rails/master_rails_by_actions/config/routes.rb
+++ rails/master_rails_by_actions/config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  root 'welcome#index'
   get 'welcome/index'
-
   resources :users
   resources :sessions
   delete '/logout' => 'sessions#destory', as: :logout
 
-  root 'welcome#index'
+  namespace :admin do
+    root 'sessions#new'
+    resources :sessions
+    resources :categories
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
