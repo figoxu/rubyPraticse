8be0538c1ac2c84abf1799416896f010a8f1dc8d
figoxu <xujh945@qq.com>
2018-06-03 08:22:35 +0800

    Change DB Configure

--- gems/activerecord/activerecord01.rb
+++ gems/activerecord/activerecord01.rb
@@ -1,13 +1,12 @@
 
 require 'active_record'
 
-
+# http://guides.rubyonrails.org/active_record_basics.html
 ActiveRecord::Base.pluralize_table_names = false
 
  class Camp < ActiveRecord::Base
-   establish_connection(adapter:"postgresql",host:"rm-xx.pg.rds.aliyuncs.com", port:3432,username:"xx",password:"xx",database:"collector")
+   establish_connection(adapter:"postgresql",host:"localhost", port:5432,username:"figo",password:"123456",database:"figo")
  end
-
 camp = Camp.first
 puts camp.servs
 puts camp.tel
